7ad8c19c904e1e54a3757da61326838f8dd98573
Berkay-Disli <berkaydisli.work@gmail.com>
2023-01-20 17:25:33 +0300

    Initial Commit

---
+++ IntroAnimation/IntroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroAnimationApp.swift
+//  IntroAnimation
+//
+//  Created by Berkay Disli on 20.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct IntroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
